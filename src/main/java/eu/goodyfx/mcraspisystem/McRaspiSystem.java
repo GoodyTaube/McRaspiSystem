5f20c92ed1a3db92a3ed1fd8573777d675304ab1
GoodyFX <goodyfxshop@gmail.com>
2024-05-27 00:36:11 +0200

    Project Main Setup

--- src/main/java/eu/goodyfx/mcraspisystem/McRaspiSystem.java
+++ src/main/java/eu/goodyfx/mcraspisystem/McRaspiSystem.java
@@ -1,14 +1,56 @@
 package eu.goodyfx.mcraspisystem;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public final class McRaspiSystem extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        //Welcome to the jungle
+        init();
     }
+
+    private void init(){
+        getLogger().info("Welcome to McRaspiSystem");
+        setupConfigs();
+        registerCommands();
+        registerEvents();
+    }
+
+    private void setupConfigs(){
+        //ALLE Config bezogenen sachen
+    }
+
+    private void registerCommands(){
+        //Alle Commands sortiert nach Wichtigkeit
+    }
+
+    private void registerEvents(){
+        //Alle Events sortiert nach Wichtigkeit
+    }
+
+    /**
+     * Setup Command Class and load.
+     * @param commandLabel The Command Name / Alias
+     * @param commandExecutor The Executor Class
+     */
+    public void setCommand(String commandLabel, CommandExecutor commandExecutor){
+        Objects.requireNonNull(getCommand(commandLabel)).setExecutor(commandExecutor);
+    }
+
+    /**
+     * Setup Listeners and load
+     * @param listeners The Listeners Class
+     */
+    public void setListeners(Listener listeners){
+        Bukkit.getPluginManager().registerEvents(listeners, this);
+    }
+
 
     @Override
     public void onDisable() {
---
+++ src/main/java/eu/goodyfx/mcraspisystem/utils/RaspiMessages.java
@@ -0,0 +1,4 @@
+package eu.goodyfx.mcraspisystem.utils;
+
+public class RaspiMessages {
+}
