440f0bde204fdf4e90b94baa66829bdaaa53c48a
GoodyFX <louisbrcko99@gmail.com>
2024-12-29 13:36:24 +0100

    fix: Code Bugs and Safety

--- src/main/java/eu/goodyfx/mcraspisystem/commands/InHeadCommand.java
+++ src/main/java/eu/goodyfx/mcraspisystem/commands/InHeadCommand.java
@@ -1,9 +1,7 @@
 package eu.goodyfx.mcraspisystem.commands;
 
 import eu.goodyfx.mcraspisystem.McRaspiSystem;
-import eu.goodyfx.mcraspisystem.utils.PlayerValues;
 import eu.goodyfx.mcraspisystem.utils.RaspiPlayer;
-import eu.goodyfx.mcraspisystem.utils.RaspiUser;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -22,9 +20,6 @@ public class InHeadCommand implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender instanceof Player player) {
             RaspiPlayer raspiPlayer = new RaspiPlayer(plugin, player);
-            if(raspiPlayer.userManager().hasTimePlayed(player, 20)){
-
-            }
 
         }
         return false;
